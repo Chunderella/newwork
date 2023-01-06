84d9a339460091698a59d6aacde24b90fbe7d072
chunderella <sijy19@naver.com>
2023-01-06 16:47:29 +0900

    test

--- git/test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    
-     public static void main(String[] args) {
-     
-        System.out.println("commit -1(bugFix)")
-        System.out.println("commit -2(main)")
-
-    }
-}
