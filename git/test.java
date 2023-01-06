5c33c7264847df7942ecaa3776a1da9201d78ffb
chunderella <sijy19@naver.com>
2023-01-06 16:52:19 +0900

    'b''

--- git/test.java
+++ git/test.java
@@ -1,9 +1,6 @@
 public class test {
     
      public static void main(String[] args) {
-     
-        System.out.println("commit -1(bugFix)")
-        System.out.println("commit -2(main)")
-
+     System.out.println("test-1")
     }
 }
